635c5ef8656e8542335a857134b5e2db321dfd67
stevecashworks <chigbustephennamdi@gmail.com>
2024-04-06 00:50:00 -0700

    edit feature was added

---
+++ routes/users/controllers/editUser.js
@@ -0,0 +1,12 @@
+const userModel= require("../../../schema.js")
+
+const editUser=async(req,res)=>{
+try {
+    const {email}=req.body
+    const updatedUser=await userModel.findOneAndUpdate({email},{$set:req.body}, {new:true})
+return res.status(200).json({success:true, result:updatedUser})
+} catch (error) {
+    return res.status(500).json({success:true,result:error.message})
+}
+}
+module.exports=editUser
--- routes/users/userRoute.js
+++ routes/users/userRoute.js
@@ -2,10 +2,12 @@ const express= require("express")
 const login = require("./controllers/login")
 const register = require("./controllers/register")
 const getAllUsers = require("./controllers/getAllUsers")
+const editUser = require("./controllers/editUser")
 const userRoute= express.Router()
 
 userRoute.post("/login",login)
 userRoute.post("/register", register)
+userRoute.post("/edit", editUser)
 userRoute.get("/", getAllUsers)
 
 
