2b1548bc6ac9450d21cf4355d5f31de52a0e7267
stevecashworks <chigbustephennamdi@gmail.com>
2024-04-05 22:15:33 -0700

    changes were mad

--- routes/users/controllers/login.js
+++ routes/users/controllers/login.js
@@ -2,10 +2,10 @@ const userModel = require("../../../schema.js");
 const login = async (req, res, next) => {
   try {
     const { email, userName, password } = req.body;
-    const userByEmail = await userModel.findOne({ email });
-    const userByName = await userModel.findOne({ userName });
-    const userExists = userByName || userByEmail;
-    if (!userExists) {
+    const thisUser = await userModel.findOne({ email });
+    
+  
+    if (!thisUser) {
       return res
         .status(404)
         .json({
@@ -13,13 +13,13 @@ const login = async (req, res, next) => {
           result: "No user was found with this details",
         });
     } else {
-      const userDetails = userByEmail ? userByEmail : userByName;
-      const password_is_correct = password === userDetails.password;
+      
+      const password_is_correct = password === thisUser.password;
       if (!password_is_correct) {
        return res.status(403).json({success:false, result:"incorrect password"})
       }
       else{
-        return res.status(200).json({success:true,result:userDetails})
+        return res.status(200).json({success:true,result:thisUser})
       }
     }
   } catch (error) {
--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
     name:{
         type:String,
         required:true,
-        unique:true
+        
     },
     email:{
         type:String,
